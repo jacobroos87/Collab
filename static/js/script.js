7759b1b7fcf3f35f71c7e1d5e2b3a9899d8ad2d4
jacobroos87 <jacobroos87@gmail.com>
2021-04-20 14:29:15 +0000

    Added content to whole site including animations

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,112 @@
 $(document).ready(function(){
-    $('.sidenav').sidenav({ edge: "right" });
-  });
+
+    // Back to top button function
+
+    const backToTopBtn = document.querySelector("#back-to-top");
+    const windowSize = $(window).width();
+
+
+    // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 80) {
+            document.getElementById("navbar").style.padding = "15px 10px";
+            document.getElementById("logo").style.width = "80px";
+            $(backToTopBtn).fadeIn("slow");
+        } else {
+            document.getElementById("navbar").style.padding = "30px 10px";
+            document.getElementById("logo").style.width = "200px";
+            $(backToTopBtn).fadeOut("slow");
+        }
+    }
+
+
+        // Slide in home elements
+
+    $.fn.visible = function(partial) {
+    
+      var $t            = $(this),
+          $w            = $(window),
+          viewTop       = $w.scrollTop(),
+          viewBottom    = viewTop + $w.height(),
+          _top          = $t.offset().top,
+          _bottom       = _top + $t.height(),
+          compareTop    = partial === true ? _bottom : _top,
+          compareBottom = partial === true ? _top : _bottom;
+    
+    return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+
+    };
+
+    $(window).scroll(function(event) {
+  
+        $(".info").each(function(i, el) {
+            var el = $(el);
+            if (el.visible(true)) {
+                setTimeout(
+                    function() 
+                    {
+                        el.addClass("come-in"); 
+                    }, 1000);
+            }
+        });
+
+
+        $("#portfolio-item-2").each(function(i, el) {
+            var el = $(el);
+            if (el.visible(true)) {
+                setTimeout(
+                    function() 
+                    {
+                        el.addClass("come-up");
+                    }, 900);
+                
+            } 
+        });
+
+        $("#portfolio-item-3").each(function(i, el) {
+            var el = $(el);
+            if (el.visible(true)) {
+                 setTimeout(
+                    function() 
+                    {
+                        el.addClass("come-in-right");
+                    }, 1000);
+            } 
+        });
+
+        $("#portfolio-item-1").each(function(i, el) {
+            var el = $(el);
+            if (el.visible(true)) {
+                 setTimeout(
+                    function() 
+                    {
+                        el.addClass("come-in-left");
+                    }, 800);
+            } 
+        });
+  
+    });
+
+    
+
+    // remove home navlink bottom border if #what_we_do is active 
+
+    var current = document.URL.split('#')[1];
+
+    if (current == 'what_we_do') {
+        $("#what-we-do-link").css("border-bottom", "5px solid white");
+        $("#home-link").css("border-bottom", "unset");
+        };
+    
+    $("#what-we-do-link").click(function() {
+        $(this).css("border-bottom", "5px solid white");
+        });
+
+
+
+
+
+});
+
